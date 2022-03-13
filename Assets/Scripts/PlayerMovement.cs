6de3a1cf8b7256396a99fe576cf1a706d69859e7
Neil111111 <danilshevchuk89@gmail.com>
2022-03-13 16:20:40 +0700

    FORVLAD

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,32 +6,42 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    public float enduranceTime = 10f;
+
+
     public float gravity = -9.8f;
     public float jumpHeight = 3f;
-    public bool isGrounded;
     Vector3 velocity;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+
     public Camera cam;
     public Transform player;
+
+    public bool isGrounded;
+    public bool isCrouched;
+
     Rigidbody rb;
+
     void Awake()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
     }
+
     void Update()
     {
         Movement();
         Crouch();
+        Sprint();
     }
+
     public void Movement()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         
         if(isGrounded && velocity.y < 0)
-
         {
             velocity.y = -2f;
         }
@@ -52,15 +62,30 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(velocity * Time.deltaTime);
     }
+
     void Crouch()
     {
         if(Input.GetButton("Crouch") && isGrounded == true)
         {
             player.transform.localScale = new Vector3 (2.5f,1.25f,2.5f);
             gravity = -120f;
+            isCrouched = true;
         }else if(Input.GetButtonUp("Crouch")){
             player.transform.localScale = new Vector3 (2.5f,2.5f,2.5f);
             gravity = -50f;
+            isCrouched = false;
         }
-    }    
+    }
+    void Sprint()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) && isGrounded && isCrouched == false && enduranceTime > 0 )
+        {
+            speed += 8f;
+            enduranceTime -= Time.deltaTime;
+            
+        }
+        else if ( enduranceTime < 10){
+            enduranceTime += Time.deltaTime;
+        }
+    }
 }
