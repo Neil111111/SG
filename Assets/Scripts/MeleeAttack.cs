5e16f4071aa9413b0a803f9f53544fbfa538130d
Neil111111 <danilshevchuk89@gmail.com>
2022-03-26 20:18:28 +0700

    CameraFix

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -6,23 +6,30 @@ using UnityEngine;
 public class MeleeAttack : MonoBehaviour
 {
     Animator anim;
+    float timer;
+    [SerializeField]public float Cooldown = 0.7f;
 
     private void Start() {
 
         anim = GetComponent<Animator>();
+        anim.SetBool("isAttacking",false);
         
     }
 
     void Update()
     {
+        timer += Time.deltaTime;
+
         Attack();
+        
     }
 
     void Attack()
-    {
-        if(Input.GetMouseButton(0))
+    {   
+        if(Input.GetMouseButtonDown(0) && timer > Cooldown)
         {
             anim.SetBool("isAttacking",true);
+            timer = 0;
         }
         if(Input.GetMouseButtonUp(0))
         {
